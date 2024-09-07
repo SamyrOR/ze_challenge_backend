160a11e41fd9125fa46991bf67d879195a7d3849
Samyr Ribeiro <saamyr@live.com>
2024-09-07 16:47:39 -0300

    feat(partner): created partner schema

---
+++ lib/ze_challenge_backend/partner/partner.ex
@@ -0,0 +1,20 @@
+defmodule ZeChallengeBackend.Partner do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "partner" do
+    field :tranding_name, :string
+    field :owner_name, :string
+    field :document, :string
+    field :coverage_area, :map
+    field :address, :map
+
+    timestamps()
+  end
+
+  def changeset(partner, attrs) do
+    partner
+    |> cast(attrs, [:tranding_name, :owner_name, :document, :coverage_area, :address])
+    |> validate_required([:tranding_name, :owner_name, :document, :coverage_area, :address])
+  end
+end
