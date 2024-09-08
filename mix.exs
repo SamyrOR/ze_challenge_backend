8703f4bd4595853af34347482c9fa69af3bb4831
Samyr Ribeiro <saamyr@live.com>
2024-09-08 15:14:15 -0300

    chore(mix): added geo package to deal with GIS

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule ZeChallengeBackend.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:geo, "~> 3.6"}
     ]
   end
 
