81d5195b79b55881a3b4d50cb37d02b561dcca19
Samyr Ribeiro <saamyr@live.com>
2024-09-27 12:47:15 -0300

    test(partner-api): done all CRUD methods test

--- test/ze_challange_backend/core/api/partner_api_test.exs
+++ test/ze_challange_backend/core/api/partner_api_test.exs
@@ -139,6 +139,64 @@ defmodule ZeChallengeBackend.Core.Partner.ApiTest do
           type: "MultiPolygon"
         }
       }
+
+      {:ok, sucess_params: params}
+    end
+
+    test "get all partners" do
+      assert {:ok, []} = Partner.Api.all()
+    end
+
+    test "insert one partner", context do
+      assert {:ok,
+              %Partner{
+                address: address,
+                trading_name: trading_name,
+                owner_name: owner_name,
+                document: document,
+                coverage_area: coverage_area
+              }} = Partner.Api.insert(context.sucess_params)
+
+      assert ^address = context.sucess_params.address
+      assert ^trading_name = context.sucess_params.trading_name
+      assert ^owner_name = context.sucess_params.owner_name
+      assert ^document = context.sucess_params.document
+      assert ^coverage_area = coverage_area
+    end
+
+    test "get all partner after insert", context do
+      for _i <- 1..10 do
+        assert {:ok, _} = Partner.Api.insert(context.sucess_params)
+      end
+
+      assert {:ok, items} = Partner.Api.all()
+      assert 10 = Enum.count(items)
+    end
+
+    test "delete a partner", context do
+      {:ok, partner} = Partner.Api.insert(context.sucess_params)
+      assert {:ok, _} = Partner.Api.delete(partner.id)
+      assert {:error, _} = Partner.Api.get(partner.id)
+    end
+
+    test "update a partner", context do
+      {:ok, partner} = Partner.Api.insert(context.sucess_params)
+
+      updated_params = %{
+        context.sucess_params
+        | trading_name: "Adega SBC",
+          owner_name: "Samyr da Ambev"
+      }
+
+      {:ok, updated_partner} = Partner.Api.update(partner, updated_params)
+
+      assert updated_partner.trading_name == updated_params.trading_name
+      assert updated_partner.owner_name == updated_params.owner_name
+    end
+
+    test "get a partner by id", context do
+      {:ok, partner} = Partner.Api.insert(context.sucess_params)
+      {:ok, _} = Partner.Api.get(partner.id)
     end
   end
 end
